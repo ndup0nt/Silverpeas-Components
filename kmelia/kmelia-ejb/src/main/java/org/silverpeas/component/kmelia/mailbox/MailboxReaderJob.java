7332be1149a6f0a592f81ee5cbc9fa200cb2ec5a
Nicolas Dupont <nicolas.dupont@aim-services.ch>
2014-02-10 18:49:08 +0100

    TMB-2 : resolve target folder in a given kmelia component from email domain

--- kmelia/kmelia-ejb/src/main/java/org/silverpeas/component/kmelia/mailbox/MailboxReaderJob.java
+++ kmelia/kmelia-ejb/src/main/java/org/silverpeas/component/kmelia/mailbox/MailboxReaderJob.java
@@ -23,8 +23,14 @@ package org.silverpeas.component.kmelia.mailbox;
 import com.silverpeas.scheduler.*;
 import com.silverpeas.scheduler.trigger.JobTrigger;
 import com.silverpeas.scheduler.trigger.TimeUnit;
+import com.silverpeas.util.StringUtil;
 import com.stratelia.silverpeas.silvertrace.SilverTrace;
+import com.stratelia.webactiv.util.EJBUtilitaire;
+import com.stratelia.webactiv.util.JNDINames;
 import com.stratelia.webactiv.util.ResourceLocator;
+import com.stratelia.webactiv.util.node.control.NodeBm;
+import com.stratelia.webactiv.util.node.model.NodeDetail;
+import com.stratelia.webactiv.util.node.model.NodePK;
 import org.apache.commons.io.IOUtils;
 
 import javax.annotation.PostConstruct;
@@ -35,6 +41,9 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A SchedulerEventListener that fires mailbox reading using MailboxReader.
@@ -45,11 +54,16 @@ import java.io.StringWriter;
 public class MailboxReaderJob implements SchedulerEventListener {
     private static final String JOB_NAME = "mailboxReaderJob";
     private static final String FREQUENCY_PROP = "kmelia.mailbox.job.frequency";
+    private static final String SUBSCRIBED_COMP_PROP = "kmelia.mailbox.job.subscribingComponentId";
+    private static final String DEFAULT_TARGET_FOLDER_PROP = "kmelia.mailbox.job.defaultTargetFolderId";
     private static final int DEFAULT_FREQUENCY = 1;
 
     private final MailboxReader mailboxReader;
     private final int frequencyInMinutes;
     private final MessageProcessor messageProcessor;
+    private final String subscribedComponentId;
+    private final String defaultTargetFolderId;
+    private NodeBm nodeBm;
 
 
     /**
@@ -59,14 +73,16 @@ public class MailboxReaderJob implements SchedulerEventListener {
      * @param kmeliaSettings   kmelia settings
      */
     @Inject
-    public MailboxReaderJob(MailboxReader theMailboxReader, ResourceLocator kmeliaSettings, MessageProcessor theMessageProcessor) {
+    public MailboxReaderJob(MailboxReader theMailboxReader, ResourceLocator kmeliaSettings,
+                            MessageProcessor theMessageProcessor) {
         this.mailboxReader = theMailboxReader;
         this.frequencyInMinutes = kmeliaSettings.getInteger(FREQUENCY_PROP, DEFAULT_FREQUENCY);
         this.messageProcessor = theMessageProcessor;
+        this.subscribedComponentId = kmeliaSettings.getString(SUBSCRIBED_COMP_PROP);
+        this.defaultTargetFolderId = kmeliaSettings.getString(DEFAULT_TARGET_FOLDER_PROP);
     }
 
     @Override
-    //TODO vérifier que le synchronized empêche l'exécution de 2 tasks en même temps
     public synchronized void triggerFired(SchedulerEvent anEvent) throws Exception {
         mailboxReader.readMailbox();
     }
@@ -88,23 +104,7 @@ public class MailboxReaderJob implements SchedulerEventListener {
      */
     @PostConstruct
     public void registerJob() throws SchedulerException {
-        mailboxReader.registerListener(new MessageListener() {
-            @Override
-            public void onMailboxRead(ReadMailboxEvent event) throws MessagingException, IOException {
-                for (Message msg : event.getMessages()) {
-                        MessageDocument doc = messageProcessor.processMessage(msg);
-                        System.out.println("Message subject : " + doc.getSubject());
-                        System.out.println("Message content : " + doc.getBody());
-                    System.out.println("Message attachments count : "+doc.getAttachments().size());
-                    for(Attachment att : doc.getAttachments()){
-                        StringWriter writer = new StringWriter();
-                        IOUtils.copy(att.getInputStream(), writer);
-                        System.out.println(att.getFileName() + " : ");
-                        System.out.println(writer.toString());
-                    }
-                }
-            }
-        });
+        registerListenersForSubscribedKmelia();
         SchedulerFactory schedulerFactory = SchedulerFactory.getFactory();
         Scheduler scheduler = schedulerFactory.getScheduler();
         if (scheduler.isJobScheduled(JOB_NAME)) {
@@ -112,5 +112,65 @@ public class MailboxReaderJob implements SchedulerEventListener {
         }
         JobTrigger trigger = JobTrigger.triggerEvery(frequencyInMinutes, TimeUnit.MINUTE);
         scheduler.scheduleJob(JOB_NAME, trigger, this);
+    }
+
+    private Map<String, NodeDetail> foldersByDescription = new HashMap<String, NodeDetail>();
+
+    private void initFoldersMapping(){
+        ArrayList<NodeDetail> subTree = getNodeBm().getSubTree(new NodePK(NodePK.ROOT_NODE_ID, subscribedComponentId));
+        for(NodeDetail node : subTree){
+            if(StringUtil.isNotBlank(node.getDescription())){
+                foldersByDescription.put(node.getDescription(), node);
+                SilverTrace.debug("kmelia", this.getClass().getName(), "Folder " + node.getName() +
+                        " will be mapped to domain "+node.getDescription());
+            }
+        }
+    }
+
+    private void registerListenersForSubscribedKmelia() {
+        initFoldersMapping();
+        mailboxReader.registerListener(new SubscribedComponentListener());
+    }
+
+    private final class SubscribedComponentListener implements MessageListener{
+        @Override
+        public void onMailboxRead(ReadMailboxEvent event) throws MessagingException, IOException {
+            for (Message msg : event.getMessages()) {
+                MessageDocument doc = messageProcessor.processMessage(msg);
+                NodeDetail targetFolder = resolveTargetFolder(doc);
+                System.out.println("Target folder : " + targetFolder.getName());
+                System.out.println("Message subject : " + doc.getSubject());
+                System.out.println("Message content : " + doc.getBody());
+                System.out.println("Message attachments count : " + doc.getAttachments().size());
+                for (Attachment att : doc.getAttachments()) {
+                    StringWriter writer = new StringWriter();
+                    IOUtils.copy(att.getInputStream(), writer);
+                    System.out.println(att.getFileName() + " : ");
+                    System.out.println(writer.toString());
+                }
+            }
+        }
+    }
+
+    private NodeDetail resolveTargetFolder(MessageDocument doc){
+        if(doc.getSender() != null){
+            int indexOfArrob = doc.getSender().indexOf('@');
+            if(indexOfArrob != -1 && indexOfArrob < doc.getSender().length() - 1){
+                String domainName = doc.getSender().substring(indexOfArrob + 1);
+                return resolveFolder(domainName);
+            }
+        }
+        return getNodeBm().getDetail(new NodePK(defaultTargetFolderId, subscribedComponentId));
+    }
+
+    private NodeDetail resolveFolder(String domainName) {
+        return foldersByDescription.get(domainName);
+    }
+
+    private NodeBm getNodeBm() {
+        if (nodeBm == null) {
+            nodeBm = EJBUtilitaire.getEJBObjectRef(JNDINames.NODEBM_EJBHOME, NodeBm.class);
+        }
+        return nodeBm;
     }
 }
